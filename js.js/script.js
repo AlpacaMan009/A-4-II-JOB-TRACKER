88e0333d3dea1438e3371f1317c4e976c136f95b
AlpacaMan009 <tahmid5678@gmail.com>
2026-03-03 03:08:24 +0600

    did some changes on the cards

--- js.js/script.js
+++ js.js/script.js
@@ -25,6 +25,7 @@ function calculateCount() {
   interviewCount.innerText = interviewList.length;
   rejectedCount.innerText = rejectedList.length;
   // allJobCount.innerText = allJobsList.length;
+  allJobCount.innerText = allCardSection.children.length + " Jobs";
 }
 
 calculateCount();
@@ -45,7 +46,6 @@ function toggleStyle(id) {
 
   currentStatus = id;
   console.log(currentStatus);
-
 
   // step 1 finish
 
@@ -76,7 +76,7 @@ mainContainer.addEventListener("click", function (event) {
     const post = parenNode.querySelector(".post").innerText;
     const details = parenNode.querySelector(".details").innerText;
     const status = parenNode.querySelector(".stat").innerText;
-    const notes = parenNode.querySelector(".note").innerText;
+    const notes = parenNode.querySelector(".notes").innerText;
 
     parenNode.querySelector(".stat").innerText = "Interview";
 
@@ -89,7 +89,7 @@ mainContainer.addEventListener("click", function (event) {
     };
 
     const jobExist = interviewList.find(
-      (item) => item.jobName == cardInfo.jobName
+      (item) => item.jobName == cardInfo.jobName,
     );
 
     if (!jobExist) {
@@ -99,27 +99,25 @@ mainContainer.addEventListener("click", function (event) {
     // step 2 finish
     // removing the plant from struggling list
     rejectedList = rejectedList.filter(
-      (item) => item.jobName != cardInfo.jobName
+      (item) => item.jobName != cardInfo.jobName,
     );
 
     // after remove rerender the html
     if (currentStatus == "rejected-filter-btn") {
       renderRejected();
-    }
-     else if (currentStatus == "interview-filter-btn") {
+    } else if (currentStatus == "interview-filter-btn") {
       renderInterview();
     }
 
     calculateCount();
-  }
-   else if (event.target.classList.contains("rejected-btn")) {
+  } else if (event.target.classList.contains("rejected-btn")) {
     const parenNode = event.target.closest(".card");
 
     const jobName = parenNode.querySelector(".jobName").innerText;
     const post = parenNode.querySelector(".post").innerText;
     const details = parenNode.querySelector(".details").innerText;
     const status = parenNode.querySelector(".stat").innerText;
-    const notes = parenNode.querySelector(".note").innerText;
+    const notes = parenNode.querySelector(".notes").innerText;
 
     parenNode.querySelector(".stat").innerText = "Rejected";
 
@@ -132,7 +130,7 @@ mainContainer.addEventListener("click", function (event) {
     };
 
     const jobExist = rejectedList.find(
-      (item) => item.jobName == cardInfo.jobName
+      (item) => item.jobName == cardInfo.jobName,
     );
 
     if (!jobExist) {
@@ -141,7 +139,7 @@ mainContainer.addEventListener("click", function (event) {
 
     // removing the plant from thriving list
     interviewList = interviewList.filter(
-      (item) => item.jobName != cardInfo.jobName
+      (item) => item.jobName != cardInfo.jobName,
     );
 
     // console.log(thrivingList);
@@ -149,8 +147,7 @@ mainContainer.addEventListener("click", function (event) {
     // after remove rerender the html
     if (currentStatus == "interview-filter-btn") {
       renderInterview();
-    }
-     else if (currentStatus == "rejected-filter-btn") {
+    } else if (currentStatus == "rejected-filter-btn") {
       renderRejected();
     }
     calculateCount();
@@ -162,97 +159,108 @@ function renderInterview() {
   // make the filterSection empty every time
   filterSection.innerHTML = "";
 
+  if (interviewList.length === 0) {
+    filterSection.innerHTML =
+      "<p class='p-4 text-center text-gray-500'>No interview jobs yet</p>";
+    return;
+  }
+
   // creating innerHtml
   for (let interview of interviewList) {
     console.log(interview);
 
     let div = document.createElement("div");
-    div.className = "flex w-full justify-between bg-slate-100 p-5 rounded-lg";
+    div.className =
+      "card flex w-full justify-between bg-slate-100 p-5 mt-5 rounded-lg";
     div.innerHTML = `
-    <div class="space-y-5">
-                    <!-- part-1 -->
-                    <div>
-                        <p class="jobName text-[18px] font-semibold text-[#002C5C]">${interview.jobName}</p>
-                        <p class="post text-neutral/50">${interview.post}</p>
+    <div class="flex-1 space-y-5">
+                    <!-- part-1 - -->
+                    <div class="flex justify-between items-start">
+                        <div>
+                            <p class="jobName text-[18px] font-semibold text-[#002C5C]">${interview.jobName}</p>
+                            <p class="post text-neutral/50">${interview.post}</p>
+                        </div>
+
+                        <!-- Delete button  -->
+                        <button id="btn-dlt" class="btn btn-outline btn-error">Delete</button>
                     </div>
 
-                    <!-- part-2 -->
+                    <!-- part-2  -->
                     <div class="flex gap-3">
-                        <p class="details text-sm text-neutral/50 font-semibold">${interview.details}</p>
-
+                        <p class="details text-sm text-neutral/50 font-semibold">
+                            ${interview.details}
+                        </p>
                     </div>
 
-                    <!-- part 3 -->
+                    <!-- part-3 -->
                     <div class="flex flex-col gap-2">
-    <p class="stat h-[36px] w-[150px] text-[#002C5C] font-semibold text-[14px]">${interview.status}</p>
-                    <p class="note">${interview.notes}</p>
+                        <p class="stat h-[36px] w-[150px] text-[#002C5C] font-semibold text-[14px]">${interview.status}</p>
+                        <p class="notes">${interview.notes}</p>
                     </div>
 
-                    
-
+                    <!-- (Interview & Rejected) buttons -->
                     <div class="flex gap-2 mt-2">
-
                         <button class="interview-btn btn btn-outline btn-success">Interview</button>
                         <button class="rejected-btn btn btn-outline btn-error">Rejected</button>
                     </div>
-
                 </div>
-
-
-                <!-- main part 2-->
-                <div>
-                    <button id="btn-dlt" class="btn btn-outline btn-error">Delete</button>
-                </div>
-            </div>
         `;
     filterSection.appendChild(div);
   }
 }
+
+
+
+
+
 
 function renderRejected() {
   // make the filterSection empty every time
   filterSection.innerHTML = "";
+
+  if (rejectedList.length === 0) {
+    filterSection.innerHTML =
+      "<p class='p-4 text-center text-gray-500'>No rejected jobs yet</p>";
+    return;
+  }
   // crating innerHtml
   for (let rejected of rejectedList) {
     let div = document.createElement("div");
-    div.className = "flex w-full justify-between bg-slate-100 p-5 rounded-lg";
+    div.className =
+      "card flex w-full justify-between bg-slate-100 p-5 mt-5 rounded-lg";
     div.innerHTML = `
-         <div class="space-y-5">
-                    <!-- part-1 -->
-                    <div>
-                        <p class="jobName text-[18px] font-semibold text-[#002C5C]">${rejected.jobName}</p>
-                        <p class="post text-neutral/50">${rejected.post}</p>
+         <div class="flex-1 space-y-5">
+                    <!-- part-1 - -->
+                    <div class="flex justify-between items-start">
+                        <div>
+                            <p class="jobName text-[18px] font-semibold text-[#002C5C]">${rejected.jobName}</p>
+                            <p class="post text-neutral/50">${rejected.post}</p>
+                        </div>
+
+                        <!-- Delete button  -->
+                        <button id="btn-dlt" class="btn btn-outline btn-error">Delete</button>
                     </div>
 
-                    <!-- part-2 -->
+                    <!-- part-2  -->
                     <div class="flex gap-3">
-                        <p class="details text-sm text-neutral/50 font-semibold">${rejected.details}</p>
-
+                        <p class="details text-sm text-neutral/50 font-semibold">
+                            ${rejected.details}
+                        </p>
                     </div>
 
-                    <!-- part 3 -->
+                    <!-- part-3 -->
                     <div class="flex flex-col gap-2">
-    <p class="stat h-[36px] w-[150px] text-[#002C5C] font-semibold text-[14px]">${rejected.status}</p>
-                    <p class="note">${rejected.notes}</p>
+                        <p class="stat h-[36px] w-[150px] text-[#002C5C] font-semibold text-[14px]">${rejected.status}</p>
+                        <p class="notes">${rejected.notes}</p>
                     </div>
 
-
+                    <!-- (Interview & Rejected) buttons -->
                     <div class="flex gap-2 mt-2">
-
                         <button class="interview-btn btn btn-outline btn-success">Interview</button>
                         <button class="rejected-btn btn btn-outline btn-error">Rejected</button>
                     </div>
-
                 </div>
-
-
-                <!-- main part 2-->
-                <div>
-                    <button id="btn-dlt" class="btn btn-outline btn-error">Delete</button>
-                </div>
-            </div>
         `;
     filterSection.appendChild(div);
   }
 }
-
